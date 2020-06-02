125ae2fc323b52df4c1536d17ab894e0390ecca8
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-02 14:23:22 -0400

    Week 4 Pair Programming Word Count MASTERED!

--- 16_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
+++ 16_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 
 namespace file_io_part1_exercises_pair
 {
@@ -6,7 +12,33 @@ namespace file_io_part1_exercises_pair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please input desired filesystem path to have amount of words and amount of sentences computed");
+            string input = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader thisDoesntMatter = new StreamReader(input))
+                {
+                    int wordCount = 0;
+
+                    while (!thisDoesntMatter.EndOfStream)
+                    {
+                        // Read in the line
+                        string line = thisDoesntMatter.ReadLine();
+                        string[] ourArrayOfSexyWords = line.Split(" ");
+
+                        wordCount += ourArrayOfSexyWords.Length;
+                    }
+                    Console.WriteLine(wordCount);
+                }
+
+            }
+            catch(IOException e) //catch a specific type of Exception
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
