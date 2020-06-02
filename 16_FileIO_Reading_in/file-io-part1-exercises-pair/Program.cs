e217d4dc3a7024392df2f76033376a23a3c2c70a
Brandon Murphy <bmurph128@gmail.com>
2020-06-02 16:56:41 -0400

    long way to go

--- 16_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
+++ 16_FileIO_Reading_in/file-io-part1-exercises-pair/Program.cs
@@ -20,25 +20,52 @@ namespace file_io_part1_exercises_pair
                 using (StreamReader thisDoesntMatter = new StreamReader(input))
                 {
                     int wordCount = 0;
+                    int sentenceCount = 0;
 
                     while (!thisDoesntMatter.EndOfStream)
                     {
                         // Read in the line
                         string line = thisDoesntMatter.ReadLine();
-                        string[] ourArrayOfSexyWords = line.Split(" ");
 
+                        string[] ourArrayOfSexyWords = line.Split(" ");
+                       
                         wordCount += ourArrayOfSexyWords.Length;
+
+
+
+                        // C:\\NicePlace\\Alice.txt to copy and paste
+
+                        
+
+                        if (line != "" && (line.Contains('.') || line.Contains('?') || line.Contains('!')))
+                        {
+
+                            string[] ourArrayOfSexySentences = line.Split(new char[] { '.', '?', '!' });
+
+                            sentenceCount += ourArrayOfSexySentences.Length;
+                        }
+
+
+                                                                                          
                     }
-                    Console.WriteLine(wordCount);
+
+                    Console.WriteLine("Word Count: " + wordCount);
+                    Console.WriteLine("Sentence Count: " + sentenceCount);
+
                 }
 
             }
-            catch(IOException e) //catch a specific type of Exception
+
+            catch (IOException e) //catch a specific type of Exception
             {
                 Console.WriteLine("Error reading the file");
                 Console.WriteLine(e.Message);
             }
+
             Console.ReadLine();
         }
     }
 }
+
+
+
