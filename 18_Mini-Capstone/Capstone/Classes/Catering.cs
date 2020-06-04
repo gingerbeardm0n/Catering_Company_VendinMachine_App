b7559acd1b9478665c09e0d629a94af94d109671
Brandon Murphy <bmurph128@gmail.com>
2020-06-04 16:50:17 -0400

    mini capstone commit 4

--- 18_Mini-Capstone/Capstone/Classes/Catering.cs
+++ 18_Mini-Capstone/Capstone/Classes/Catering.cs
@@ -30,6 +30,30 @@ namespace Capstone.Classes
             FileAccess fa = new FileAccess();
             return fa.ReadItems();
         }
-   
+
+
+
+
+        //methods
+
+        //switch case 2.2 in UserInterface
+
+        //if product code does not exist, customer is informed and return to Purchase menu
+        //if product is sold out, the customer is informed and returned to the Purchase menu
+        //if not enough of the product is in stock for the amount the customer requested, inform there is insufficient stock
+        //if a valid product is selected it is marked as purchased (???) //log purchase
+        //after the product is purchased, the balance should be updated accordingly and the customer returned to Purchase menu
+
+
+
+        //switch case 2.3 in UserInterface
+
+        //change given - log each time - see README
+        //balance set to 0
+        //report displayed that shows items purchased, amount of each item and the total cost for those items and total amount for order
+        
+
+        //new log class: log money added, change given, each purchase
+
     }
 }
