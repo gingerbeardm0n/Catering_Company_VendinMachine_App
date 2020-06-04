3ec1bf83bf4c8fe898285e9cba4cc83ea666d90e
Brandon Murphy <bmurph128@gmail.com>
2020-06-04 13:12:14 -0400

    first commit for pair work

--- 18_Mini-Capstone/Capstone/Classes/FileAccess.cs
+++ 18_Mini-Capstone/Capstone/Classes/FileAccess.cs
@@ -7,5 +7,9 @@ namespace Capstone.Classes
     public class FileAccess
     {
         // This class should contain any and all details of access to files
+        
+        private string fullPath = @"c:\Catering\cateringsystem.csv";
+
+
     }
 }
