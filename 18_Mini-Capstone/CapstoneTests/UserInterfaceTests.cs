e63d1886faaf27e5914c78e68c1a8c60200efed1
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-04 20:38:43 -0400

    Capstone - Creat UserInterfaceTests class

---
+++ 18_Mini-Capstone/CapstoneTests/UserInterfaceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class UserInterfaceTests
+    {
+        UserInterface testObj;
+
+        [TestInitialize]
+        public void Init()
+        {
+            testObj = new UserInterface();
+        }
+
+       
+        
+
+
+    }
+}
